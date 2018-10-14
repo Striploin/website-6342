85e36f5951da43db5a1bf674130362bf87a56d1b
Zach <zachdonovan5@gmail.com>
2018-10-14 13:05:34 -0400

    Fixed PHP redirect w/ JS popup window on email send

--- assets/php/send.php
+++ assets/php/send.php
@@ -1,52 +1,14 @@
 <?php
-session_cache_limiter('nocache');
-header('Expires: ' . gmdate('r', 0));
-header('Content-type: application/json');
-$to = 'team@6342.ca';
-$subject = $_POST['subject'];
-if (isset($_POST['email'])) {
-                $name = $_POST['name'];
-                $email = $_POST['email'];             
-                $fields = array(
-                                0 => array(       
-                                                'text' => 'Name',
-                                                'val' => $_POST['name']
-                                ),
-                                1 => array(
-                                                
-                                                'text' => 'Email address',
-                                                'val' => $_POST['email']
-                                ),
-                                2 => array(
-                                                'text' => 'Message',
-                                                'val' => $_POST['message']  
-                                )    
-                );
-                $message = "";
-                foreach ($fields as $field) {
-                                $message .= $field['text'] . ": " . htmlspecialchars($field['val'], ENT_QUOTES) . "<br>\n";
-                }
-                $headers = '';
-                $headers .= 'From: ' . $name . ' <' . $email . '>' . "\r\n";
-                $headers .= "Reply-To: " . $email . "\r\n";
-                $headers .= "MIME-Version: 1.0\r\n";
-                $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-                if (mail($to, $subject, $message, $headers)) {
-                                $arrResult = array(
-                                                'response' => 'success'
-                                );
-                } else {
-                                $arrResult = array(
-                                                'response' => 'error'
-                                );
-                }
-                
-                echojson_encode($arrResult);
-} else {
-                $arrResult = array(
-                                'response' => 'error'
-                );
-                echojson_encode($arrResult);
-                
-}
+//if "email" variable is filled out, send email
+  if (isset($_REQUEST['email']))  {
+  //Email information
+  $admin_email = "team@6342.ca";
+  $email = $_REQUEST['email'];
+  $message = $_REQUEST['message'];
+  $subject = "New Form Reply:";
+  
+  //send email
+  mail($admin_email,"$subject", $message, "From:" . $email);
+   header( "Location: ../../" );
+  }
 ?>
